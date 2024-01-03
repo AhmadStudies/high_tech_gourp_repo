72c3f4698ab76a2b27ca530cfebeef620135d869
Elyas-Baheen <elyasahmadbaheen008@gmail.com>
2024-01-03 13:06:00 +0430

    created new changes to Elyas_A_Baheen

--- src/main/java/Book.java
+++ src/main/java/Book.java
@@ -4,4 +4,18 @@ public class Book {
 
     private int version;
     private String ahmad;
+
+    public void setAhmad(String ahmad){
+        this.ahmad = ahmad;
+    }
+    public String getAhmad(){
+        return ahmad;
+    }
+    private String name;
+    public void setName(String name){
+
+    }
+    public String getName(){
+        return name;
+    }
 }
