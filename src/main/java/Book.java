22ae54f54edd24634755841ba20755623f31479e
Elyas-Baheen <elyasahmadbaheen008@gmail.com>
2024-01-09 23:05:43 +0430

    created new changes successfully

--- src/main/java/Book.java
+++ src/main/java/Book.java
@@ -1,7 +1,8 @@
 public class Book {
-    public static void main (String [] args){
-    int year = 2024;
+    public static void main(String[] args) {
+        int year = 2024;
     }
+
     private String bookName;
     private String author;
 
@@ -10,40 +11,48 @@ public class Book {
     private String mahmod;
 
     private String Elyas;
-    public void setElyas(String Elyas){
+
+    public void setElyas(String Elyas) {
         this.Elyas = Elyas;
     }
-    public String getElyas(){
+
+    public String getElyas() {
         return Elyas;
     }
 
-    public void setAhmad(String ahmad){
+    public void setAhmad(String ahmad) {
         this.ahmad = ahmad;
     }
-    public String getAhmad(){
+
+    public String getAhmad() {
         return ahmad;
     }
 
-    public void setBookName(String ahmad){
+    public void setBookName(String ahmad) {
         this.bookName = bookName;
     }
-    public String getBookName(){
+
+    public String getBookName() {
         return bookName;
     }
 
-    public void setAuthor(String author){
+    public void setAuthor(String author) {
         this.author = author;
     }
 
-    public void setMahmod(String mahmod){
+    public void setMahmod(String mahmod) {
         this.mahmod = mahmod;
     }
 
-    public String getMahmod(){
+    public String getMahmod() {
         return mahmod;
     }
-    public void print(){
+
+    public void print() {
         System.out.println(mahmod);
+    }
+    public String Elyas(String Elyas) {
+        return Elyas;
     }
 
 
